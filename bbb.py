5d389f6e488d5260999a72a7a44958d162bb6764
Ostlund11 <bernardostlundbruna@gmail.com>
2021-09-13 14:06:44 +0200

    new interest

--- bbb.py
+++ bbb.py
@@ -1,3 +1,13 @@
 first_name = "Bernard"
 last_name = "Bruna"
 print(f"{first_name} {last_name}")
+
+amount = 100 # befintlig summa
+increase = 1.2 # ökningen 
+total = amount * increase ** 99 # uträkning samt antal år
+print (total) # här skrivs variabeln "total" ut
+
+years = int(input("How many years?"))
+
+total_amount = amount * increase ** years
+print(total_amount)
