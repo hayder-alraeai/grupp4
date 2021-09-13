07d65e84cff2675388a6407fa4c7c391b49684c8
Alva Thunberg <alvathunberg@icloud.com>
2021-09-13 11:39:08 +0200

    test

---
+++ alt.py
@@ -0,0 +1,11 @@
+name = 'My name is Alva'
+print(name)
+
+
+
+#Går också att genomföra med hjälp av funktioner
+def greet(user_name):
+    print(f"My name is {user_name}")
+
+greet(input("Enter your name: "))
+
